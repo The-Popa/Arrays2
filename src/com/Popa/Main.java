93fb5c52d210290f4d7de8d2e0389b5d296d9b48
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-25 08:40:00 +0200

    Array1

---
+++ src/com/Popa/Main.java
@@ -0,0 +1,11 @@
+package com.Popa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] numbers = {11, 23, 39};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
